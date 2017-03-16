75cd6cec3d61ce867d324c154178b52d0a82a01d
daywrite <daywrite@yeah.net>
2017-03-16 18:54:43 +0800

    引入一个基于bootstrap3的前端框架

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App'
 
 /* 全局引入jquery */
-// import $ from 'jquery'
+import 'jquery'
+/* 引入字体图标 */
+import './assets/font-awesome/css/font-awesome.css'
 /* 引入bootstrap */
 import './assets/css/bootstrap.css'
 import './assets/js/bootstrap'
-
+/* 引入框架CSS、侧面菜单css、js */
+import './assets/css/all.css'
+import './assets/css/metisMenu/metisMenu.css'
+import './assets/css/metisMenu/metisMenu'
+/* $(document).ready() */
+import './assets/js/all'
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
